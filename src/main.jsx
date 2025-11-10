07e3bdb19f4a7c96bbcd9121f1a94ea32cb48da1
akhil <events@mintlabs.in>
2025-11-10 18:29:58 +0530

    removed gifts

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'  // Make sure this path is correct
+import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist',
+    sourcemap: false,
+    chunkSizeWarningLimit: 1600,
   },
   server: {
-    historyApiFallback: true,
-  },
-});
+    port: 3000,
+  }
+})
